8010f807720183d0845ee8e8b2d30f4003e1374b
Jonathan Scaramal <jonathan.scaramal@mercadolibre.com>
2020-10-29 15:03:21 -0300

    chore: Created CustomPXDynamicComponent

---
+++ ExampleSwift/CustomPXDynamicComponent.swift
@@ -0,0 +1,9 @@
+//
+//  CustomPXDynamicComponent.swift
+//  ExampleSwift
+//
+//  Created by Jonathan Scaramal on 28/10/2020.
+//  Copyright © 2020 Juan Sebastian Sanzone. All rights reserved.
+//
+
+import Foundation
