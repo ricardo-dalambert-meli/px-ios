1e634c661fc8a9de4ceebca3309002af260a8a81
Jona <jonathan.scaramal@mercadolibre.com>
2021-01-04 18:39:22 -0300

    Update ViewController.swift

--- ExampleSwift/ExampleSwift/Controllers/ViewController.swift
+++ ExampleSwift/ExampleSwift/Controllers/ViewController.swift
@@ -32,8 +32,7 @@ class ViewController: UIViewController {
 
     private func runMercadoPagoCheckout() {
         // 1) Create Builder with your publicKey and preferenceId.
-        let builder = MercadoPagoCheckoutBuilder(publicKey: "TEST-e28d5a35-dece-45c9-9618-e8cc5dec6c42", preferenceId: "656525290-7bda964b-26d9-4352-a04c-1b04801627ee").setLanguage("es")
-        builder.setPrivateKey(key: "TEST-7169122440478352-062213-d23fa9fb38e4b3e94feee29864f0fae2-443064294")
+        let builder = MercadoPagoCheckoutBuilder(publicKey: "TEST-4763b824-93d7-4ca2-a7f7-93539c3ee5bd", preferenceId: "243966003-0812580b-6082-4104-9bce-1a4c48a5bc44").setLanguage("es")
 
         // 2) Create Checkout reference
         checkout = MercadoPagoCheckout(builder: builder)
@@ -47,7 +46,6 @@ class ViewController: UIViewController {
     private func runMercadoPagoCheckoutWithLifecycle() {
         // 1) Create Builder with your publicKey and preferenceId.
         let builder = MercadoPagoCheckoutBuilder(publicKey: "TEST-4763b824-93d7-4ca2-a7f7-93539c3ee5bd", preferenceId: "243966003-0812580b-6082-4104-9bce-1a4c48a5bc44").setLanguage("es")
-        builder.setPrivateKey(key: "TEST-2419260407828239-101317-ea55bf70890b2101b8f7c30bf5689891-509936543")
 
         // 2) Create Checkout reference
         checkout = MercadoPagoCheckout(builder: builder)
