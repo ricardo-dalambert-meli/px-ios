4ad7af6bb76f7c932a5a84088c7cced269a30bf6
jscaramal-meli <jonathan.scaramal@mercadolibre.com>
2021-01-06 15:29:06 -0300

    chore: removed commented lines

--- ExampleObjectiveC/MercadoPagoSDKExamplesObjectiveC/MercadoPagoSDKExamplesObjectiveC-Bridging-Header.h
+++ ExampleObjectiveC/MercadoPagoSDKExamplesObjectiveC/MercadoPagoSDKExamplesObjectiveC-Bridging-Header.h
@@ -2,8 +2,4 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-//#if PX_PRIVATE_POD
 @import MercadoPagoSDKV4;
-//#else
-//@import MercadoPagoSDK;
-//#endif
