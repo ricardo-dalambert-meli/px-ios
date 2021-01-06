c7cd3f250ae9a74894e343f3848f553c96383111
jscaramal-meli <jonathan.scaramal@mercadolibre.com>
2021-01-06 15:20:02 -0300

    fix: Fixed some errors from merge and get ExampleObjectiveC working with MercadoPagoSDKV4

--- ExampleObjectiveC/MercadoPagoSDKExamplesObjectiveC/MercadoPagoSDKExamplesObjectiveC-Bridging-Header.h
+++ ExampleObjectiveC/MercadoPagoSDKExamplesObjectiveC/MercadoPagoSDKExamplesObjectiveC-Bridging-Header.h
@@ -2,8 +2,8 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#if PX_PRIVATE_POD
+//#if PX_PRIVATE_POD
 @import MercadoPagoSDKV4;
-#else
-@import MercadoPagoSDK;
-#endif
+//#else
+//@import MercadoPagoSDK;
+//#endif
