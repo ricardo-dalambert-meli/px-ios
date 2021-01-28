477176d557cf2bccb73cf3e9faed0e0d38908ee8
Vinicius de Andrade <vinicius.andrade@mercadolivre.com>
2021-01-28 12:13:04 -0300

    Working AndesUI/AndesBottomSheet

--- MercadoPagoSDK/MercadoPagoSDK/Flows/OneTap/UI/PXOneTapViewController.swift
+++ MercadoPagoSDK/MercadoPagoSDK/Flows/OneTap/UI/PXOneTapViewController.swift
@@ -44,7 +44,7 @@ final class PXOneTapViewController: PXComponentContainerViewController {
     var cardSliderMarginConstraint: NSLayoutConstraint?
     private var navigationBarTapGesture: UITapGestureRecognizer?
     var installmentRow = PXOneTapInstallmentInfoView()
-//    private var andesBottomSheet: AndesBottomSheetViewController?
+    private var andesBottomSheet: AndesBottomSheetViewController?
 
     // MARK: Lifecycle/Publics
     init(viewModel: PXOneTapViewModel, timeOutPayButton: TimeInterval = 15, callbackPaymentData : @escaping ((PXPaymentData) -> Void), callbackConfirm: @escaping ((PXPaymentData, Bool) -> Void), callbackUpdatePaymentOption: @escaping ((PaymentMethodOption) -> Void), callbackRefreshInit: @escaping ((String) -> Void), callbackExit: @escaping (() -> Void), finishButtonAnimation: @escaping (() -> Void)) {
@@ -659,18 +659,18 @@ extension PXOneTapViewController: PXCardSliderProtocol {
         }
     }
 
-//    private func buildBottomSheet(newCard: PXOneTapNewCardDto) -> AndesBottomSheetViewController {
-//        if let andesBottomSheet = andesBottomSheet {
-//            return andesBottomSheet
-//        }
-//        let viewController = PXOneTapSheetViewController(newCard: newCard)
-//        viewController.delegate = self
-//        let sheet = AndesBottomSheetViewController(rootViewController: viewController)
-//        sheet.titleBar.text = newCard.label.message
-//        sheet.titleBar.textAlignment = .center
-//        andesBottomSheet = sheet
-//        return sheet
-//    }
+    private func buildBottomSheet(newCard: PXOneTapNewCardDto) -> AndesBottomSheetViewController {
+        if let andesBottomSheet = andesBottomSheet {
+            return andesBottomSheet
+        }
+        let viewController = PXOneTapSheetViewController(newCard: newCard)
+        viewController.delegate = self
+        let sheet = AndesBottomSheetViewController(rootViewController: viewController)
+        sheet.titleBar.text = newCard.label.message
+        sheet.titleBar.textAlignment = .center
+        andesBottomSheet = sheet
+        return sheet
+    }
 
     private func addNewCard(initType: String? = "standard") {
         let siteId = viewModel.siteId
@@ -716,9 +716,9 @@ extension PXOneTapViewController: PXCardSliderProtocol {
 
 extension PXOneTapViewController: PXOneTapSheetViewControllerProtocol {
     func didTapOneTapSheetOption(sheetOption: PXOneTapSheetOptionsDto) {
-//        andesBottomSheet?.dismiss(animated: true, completion: { [weak self] in
-//            self?.addNewCard(initType: sheetOption.cardFormInitType)
-//        })
+        andesBottomSheet?.dismiss(animated: true, completion: { [weak self] in
+            self?.addNewCard(initType: sheetOption.cardFormInitType)
+        })
     }
 }
 
