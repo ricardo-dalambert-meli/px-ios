e0dfd4807361c5b219bf47e081e98657a2ee9137
Matheus <matheusl.martins@mercadolivre.com>
2021-03-08 17:40:13 -0300

    Adjusting Encode on PXCheckoutStore to add new propertie

--- MercadoPagoSDK/MercadoPagoSDK/Core/PXCheckoutStore+Encodable.swift
+++ MercadoPagoSDK/MercadoPagoSDK/Core/PXCheckoutStore+Encodable.swift
@@ -14,11 +14,13 @@ extension PXCheckoutStore: Encodable {
         try container.encodeIfPresent(self.checkoutPreference?.id, forKey: .checkoutPreference)
         try container.encodeIfPresent(self.checkoutPreference?.merchantOrderId, forKey: .merchantOrderId)
         try container.encodeIfPresent(self.paymentDatas, forKey: .paymentDatas)
+        try container.encodeIfPresent(self.validationProgramId, forKey: .validationProgramId)
     }
 
     enum PXCheckoutStoreCodingKeys: String, CodingKey {
         case checkoutPreference = "pref_id"
         case merchantOrderId = "merchant_order_id"
         case paymentDatas = "payment_data"
+        case validationProgramId = "validationProgramId"
     }
 }
