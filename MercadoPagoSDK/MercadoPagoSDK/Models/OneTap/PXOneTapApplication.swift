d98f7a6ed1db0471ee6c4893e1c8f38d33d37879
Matheus <matheusl.martins@mercadolivre.com>
2021-03-16 14:36:32 -0300

    Add new propertie on PXOneTapDto

--- MercadoPagoSDK/MercadoPagoSDK/Models/OneTap/PXOneTapApplication.swift
+++ MercadoPagoSDK/MercadoPagoSDK/Models/OneTap/PXOneTapApplication.swift
@@ -14,10 +14,24 @@ public struct PXApplicationValidationProgram: Codable {
     var mandatory: Bool
 }
 
+public struct PXApplicationStatus: Codable {
+    var enabled: Bool
+    var mainMessage: PXText?
+    var secondaryMessage: PXText?
+    var detail: String
+
+    public enum CodingKeys: String, CodingKey {
+        case enabled = "enabled"
+        case mainMessage = "main_message"
+        case secondaryMessage = "secondary_message"
+        case detail = "detail"
+    }
+}
+
 public struct PXOneTapApplication: Codable {
     var paymentMethod: PXApplicationPaymentMethod
     var validationPrograms: [PXApplicationValidationProgram]
-    var status: PXStatus
+    var status: PXApplicationStatus
 
     public enum CodingKeys: String, CodingKey {
         case paymentMethod = "payment_method"
