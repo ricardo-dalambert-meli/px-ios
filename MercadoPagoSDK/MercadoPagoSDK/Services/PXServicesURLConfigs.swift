55c4a31057e662bb10c063c8f929085d67a1843b
jscaramal-meli <jonathan.scaramal@mercadolibre.com>
2021-03-29 11:19:39 -0300

    fix: Removed wrong changes

--- MercadoPagoSDK/MercadoPagoSDK/Services/PXServicesURLConfigs.swift
+++ MercadoPagoSDK/MercadoPagoSDK/Services/PXServicesURLConfigs.swift
@@ -25,7 +25,7 @@ internal class PXServicesURLConfigs {
     
     static let API_VERSION = "2.0"
     
-    static let MP_API_BASE_URL: String = ""//"https://api.mercadopago.com"
+    static let MP_API_BASE_URL: String = "https://api.mercadopago.com"
 
     static let MP_DEFAULT_PROCESSING_MODE = "aggregator"
     static let MP_DEFAULT_PROCESSING_MODES = [MP_DEFAULT_PROCESSING_MODE]
@@ -67,7 +67,7 @@ internal class PXServicesURLConfigs {
         self.MP_REMEDY_URI = NEW_API_SELECTED_ENV + "/px_mobile/v1/remedies/${payment_id}"
         self.MP_INSTRUCTIONS_URI = MP_SELECTED_ENV  + "/checkout/payments/${payment_id}/results"
         self.MP_PAYMENTS_URI = MP_SELECTED_ENV + "/px_mobile/payments"
-        self.MP_INIT_URI = "https://run.mocky.io/v3/3b30fe9d-6c1c-498d-ae22-2d0ef1cf2c64"//NEW_API_SELECTED_ENV + "/px_mobile/v2/checkout"
+        self.MP_INIT_URI = NEW_API_SELECTED_ENV + "/px_mobile/v2/checkout"
         self.MP_RESET_ESC_CAP = NEW_API_SELECTED_ENV + "/px_mobile/v1/esc_cap"
         self.MP_POINTS_URI = MP_SELECTED_ENV + "/px_mobile/congrats"
     }
