b4a83a4282a59e833cd51cbd46370c7894610cbc
Matheus <matheusl.martins@mercadolivre.com>
2021-05-10 10:51:25 -0300

    Adjusting InstructionView to remap Its design according to backend Answer

--- MercadoPagoSDK/MercadoPagoSDK/UI/PXResult/New UI/PixCode/InstructionView.swift
+++ MercadoPagoSDK/MercadoPagoSDK/UI/PXResult/New UI/PixCode/InstructionView.swift
@@ -15,42 +15,11 @@ final class InstructionView: UIView {
         return label
     }()
     
-    private let subtitleLabel: UILabel = {
-        let label = UILabel()
-        label.font = UIFont.ml_regularSystemFont(ofSize: 16)
-        label.numberOfLines = 0
-        return label
-    }()
-    
-    private let codeBorderView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .clear
-        view.layer.borderColor = UIColor.black.withAlphaComponent(0.05).cgColor
-        view.layer.borderWidth = 1
-        view.layer.cornerRadius = 6
-        return view
-    }()
-    
-    private let codeLabel: UILabel = {
-        let label = UILabel()
-        label.font = UIFont.ml_semiboldSystemFont(ofSize: 16)
-        label.textColor = UIColor.black.withAlphaComponent(0.45)
-        label.textAlignment = .left
-        return label
-    }()
-    
-    private let copyCodeButton: UIButton = {
-        let button = UIButton()
-        button.titleLabel?.font = UIFont.ml_semiboldSystemFont(ofSize: 14)
-        button.setTitleColor(UIColor(red: 52, green: 131, blue: 250), for: .normal)
-        return button
-    }()
-    
     private let stepsStack: UIStackView = {
         let stack = UIStackView()
         stack.axis = .vertical
         stack.alignment = .leading
-        stack.spacing = 8
+        stack.spacing = 16
         return stack
     }()
     
@@ -67,12 +36,6 @@ final class InstructionView: UIView {
         return label
     }()
     
-    private let separatorView: UIView = {
-        let view = UIView()
-        view.backgroundColor = UIColor.black.withAlphaComponent(0.1)
-        return view
-    }()
-    
     // MARK: - Initialization
     init(instruction: PXInstruction) {
         super.init(frame: .zero)
@@ -87,13 +50,8 @@ final class InstructionView: UIView {
     // MARK: - Private methods
     private func setupInfos(with instruction: PXInstruction) {
         titleLabel.attributedText = instruction.subtitle?.htmlToAttributedString?.with(font: titleLabel.font)
-        subtitleLabel.attributedText = instruction.interactions?.first?.title?.htmlToAttributedString?.with(font: subtitleLabel.font)
-        codeLabel.text = instruction.interactions?.first?.content
-        codeLabel.numberOfLines = 2
-        copyCodeButton.setTitle(instruction.interactions?.first?.action?.label, for: .normal)
         
-        instruction.interactions?.enumerated().forEach { index, interaction in
-            if index == 0 { return }
+        instruction.interactions?.forEach { interaction in
             addVariableComponents(interaction: interaction)
         }
         
@@ -101,62 +59,16 @@ final class InstructionView: UIView {
     }
     
     private func addVariableComponents(interaction: PXInstructionInteraction) {
-        if let title = interaction.title, let content = interaction.content, let action = interaction.action {
-            let instructionLabel: UILabel = {
-                let label = UILabel()
-                label.font = UIFont.ml_regularSystemFont(ofSize: 16)
-                label.attributedText = title.htmlToAttributedString?.with(font: UIFont.ml_regularSystemFont(ofSize: 16))
-                label.numberOfLines = 0
-                return label
-            }()
-            
-            let codeLabel: UILabel = {
-                let label = UILabel()
-                label.font = UIFont.ml_semiboldSystemFont(ofSize: 16)
-                label.textColor = UIColor.black.withAlphaComponent(0.45)
-                label.textAlignment = .center
-                label.text = content
-                label.layer.borderColor = UIColor.black.withAlphaComponent(0.05).cgColor
-                label.layer.borderWidth = 1
-                label.layer.cornerRadius = 6
-                label.numberOfLines = 1
-                return label
-            }()
-            
-            let actionButton: UIButton = {
-                let button = UIButton()
-                button.titleLabel?.font = UIFont.ml_semiboldSystemFont(ofSize: 14)
-                button.setTitle(action.label, for: .normal)
-                button.setTitleColor(UIColor(red: 52, green: 131, blue: 250), for: .normal)
-                return button
-            }()
-            
-            stepsStack.addArrangedSubviews(views: [instructionLabel, codeLabel, actionButton])
-        } else if let title = interaction.title, let action = interaction.action {
-            let instructionLabel: UILabel = {
-                let label = UILabel()
-                label.attributedText = title.htmlToAttributedString?.with(font: UIFont.ml_regularSystemFont(ofSize: 16))
-                label.numberOfLines = 0
-                return label
-            }()
-            
-            let actionButton: UIButton = {
-                let button = UIButton()
-                button.titleLabel?.textAlignment = .left
-                button.titleLabel?.font = UIFont.ml_semiboldSystemFont(ofSize: 14)
-                button.setTitle(action.label, for: .normal)
-                button.setTitleColor(UIColor(red: 52, green: 131, blue: 250), for: .normal)
-                return button
-            }()
-
-            stepsStack.addArrangedSubviews(views: [instructionLabel, actionButton])
+        if let _ = interaction.title, let _ = interaction.content, let _ = interaction.action {
+            stepsStack.addArrangedSubviews(views: [InstructionActionView(instruction: interaction)])
         } else if let title = interaction.title {
             let instructionLabel: UILabel = {
                 let label = UILabel()
+                label.attributedText = title.htmlToAttributedString?.with(font: UIFont.ml_regularSystemFont(ofSize: 16))
                 label.numberOfLines = 0
                 return label
             }()
-            instructionLabel.attributedText = title.htmlToAttributedString?.with(font: UIFont.ml_regularSystemFont(ofSize: 16))
+            
             stepsStack.addArrangedSubviews(views: [instructionLabel])
         }
     }
@@ -164,8 +76,7 @@ final class InstructionView: UIView {
 
 extension InstructionView: ViewConfiguration {
     func buildHierarchy() {
-        addSubviews(views: [titleLabel, subtitleLabel, codeBorderView, copyCodeButton, stepsStack, watchIcon, footerLabel, separatorView])
-        codeBorderView.addSubviews(views: [codeLabel])
+        addSubviews(views: [titleLabel, stepsStack, watchIcon, footerLabel])
     }
     
     func setupConstraints() {
@@ -174,24 +85,7 @@ extension InstructionView: ViewConfiguration {
             titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 32),
             titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -32),
             
-            subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 24),
-            subtitleLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
-            subtitleLabel.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor),
-            
-            codeBorderView.topAnchor.constraint(equalTo: subtitleLabel.bottomAnchor, constant: 8),
-            codeBorderView.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
-            codeBorderView.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor),
-            
-            codeLabel.topAnchor.constraint(equalTo: codeBorderView.topAnchor, constant: 16),
-            codeLabel.leadingAnchor.constraint(equalTo: codeBorderView.leadingAnchor, constant: 16),
-            codeLabel.trailingAnchor.constraint(equalTo: codeBorderView.trailingAnchor, constant: -16),
-            codeLabel.bottomAnchor.constraint(equalTo: codeBorderView.bottomAnchor, constant: -16),
-            
-            copyCodeButton.topAnchor.constraint(equalTo: codeBorderView.bottomAnchor, constant: 6),
-            copyCodeButton.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
-            copyCodeButton.heightAnchor.constraint(equalToConstant: 18),
-            
-            stepsStack.topAnchor.constraint(equalTo: copyCodeButton.bottomAnchor, constant: 18),
+            stepsStack.topAnchor.constraint(equalTo: titleLabel.bottomAnchor),
             stepsStack.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
             stepsStack.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor),
             
@@ -203,12 +97,7 @@ extension InstructionView: ViewConfiguration {
             footerLabel.topAnchor.constraint(equalTo: stepsStack.bottomAnchor, constant: 24),
             footerLabel.leadingAnchor.constraint(equalTo: watchIcon.trailingAnchor, constant: 8),
             footerLabel.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor),
-            
-            separatorView.topAnchor.constraint(equalTo: footerLabel.bottomAnchor, constant: 24),
-            separatorView.leadingAnchor.constraint(equalTo: leadingAnchor),
-            separatorView.trailingAnchor.constraint(equalTo: trailingAnchor),
-            separatorView.bottomAnchor.constraint(equalTo: bottomAnchor),
-            separatorView.heightAnchor.constraint(equalToConstant: 1)
+            footerLabel.bottomAnchor.constraint(equalTo: bottomAnchor)
         ])
     }
     
---
+++ MercadoPagoSDK/MercadoPagoSDK/UI/PXResult/New UI/PixCode/Views/InstructionActionView.swift
@@ -0,0 +1,99 @@
+//
+//  InstructionActionView.swift
+//  AndesUI
+//
+//  Created by Matheus Leandro Martins on 10/05/21.
+//
+
+import UIKit
+
+final class InstructionActionView: UIView {
+    // MARK: - Private properties
+    private let mainStack: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.alignment = .leading
+        stack.spacing = 8
+        return stack
+    }()
+    
+    private let instructionLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.ml_regularSystemFont(ofSize: 16)
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private let codeBorderView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .clear
+        view.layer.borderColor = UIColor.black.withAlphaComponent(0.05).cgColor
+        view.layer.borderWidth = 1
+        view.layer.cornerRadius = 6
+        return view
+    }()
+    
+    private let codeLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.ml_semiboldSystemFont(ofSize: 16)
+        label.textColor = UIColor.black.withAlphaComponent(0.45)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let actionButton: UIButton = {
+        let button = UIButton()
+        button.titleLabel?.font = UIFont.ml_semiboldSystemFont(ofSize: 14)
+        button.setTitleColor(UIColor(red: 52, green: 131, blue: 250), for: .normal)
+        return button
+    }()
+    
+    // MARK: - Initialization
+    init(instruction: PXInstructionInteraction) {
+        super.init(frame: .zero)
+        setupInfos(with: instruction)
+        setupViewConfiguration()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Private methods
+    private func setupInfos(with instruction: PXInstructionInteraction) {
+        instructionLabel.attributedText = instruction.title?.htmlToAttributedString?.with(font: instructionLabel.font)
+        codeBorderView.isHidden = instruction.content == nil || instruction.content == ""
+        codeLabel.numberOfLines = instruction.isBoleto ? 2 : 1
+        codeLabel.text = instruction.content
+        actionButton.setTitle(instruction.action?.label, for: .normal)
+    }
+}
+
+extension InstructionActionView: ViewConfiguration {
+    func buildHierarchy() {
+        addSubviews(views: [mainStack])
+        codeBorderView.addSubviews(views: [codeLabel])
+        mainStack.addArrangedSubviews(views: [instructionLabel, codeBorderView, actionButton])
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            mainStack.topAnchor.constraint(equalTo: topAnchor),
+            mainStack.leadingAnchor.constraint(equalTo: leadingAnchor),
+            mainStack.trailingAnchor.constraint(equalTo: trailingAnchor),
+            mainStack.bottomAnchor.constraint(equalTo: bottomAnchor),
+            
+            codeBorderView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            codeBorderView.trailingAnchor.constraint(equalTo: trailingAnchor),
+
+            codeLabel.topAnchor.constraint(equalTo: codeBorderView.topAnchor, constant: 8),
+            codeLabel.leadingAnchor.constraint(equalTo: codeBorderView.leadingAnchor, constant: 16),
+            codeLabel.trailingAnchor.constraint(equalTo: codeBorderView.trailingAnchor, constant: -16),
+            codeLabel.bottomAnchor.constraint(equalTo: codeBorderView.bottomAnchor, constant: -8),
+        ])
+    }
+    
+    func viewConfigure() {
+        backgroundColor = .white
+    }
+}
