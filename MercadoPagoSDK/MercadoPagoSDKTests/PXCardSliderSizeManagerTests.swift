a09ff7d9ae06d0f3eab5dca7e911eebe0d209236
Jona <jonathan.scaramal@mercadolibre.com>
2021-07-21 15:59:46 -0300

    chore: Updated Tests with V3 card types

--- MercadoPagoSDK/MercadoPagoSDKTests/PXCardSliderSizeManagerTests.swift
+++ MercadoPagoSDK/MercadoPagoSDKTests/PXCardSliderSizeManagerTests.swift
@@ -14,87 +14,87 @@ class PXCardSliderSizeManagerTests: XCTestCase {
     func testGetCardTypeForSmallWithEverything () {
         let cardType = PXCardSliderSizeManager.getCardTypeForContext(deviceSize: PXDeviceSize.Sizes.small, hasCharges: true, hasDiscounts: true, hasInstallments: true, hasSplit: true)
         
-        XCTAssertEqual(cardType, MLCardDrawerType.small)
+        XCTAssertEqual(cardType, MLCardDrawerTypeV3.small)
     }
     
     func testGetCardTypeForSmallWithNothing () {
         let cardType = PXCardSliderSizeManager.getCardTypeForContext(deviceSize: PXDeviceSize.Sizes.small, hasCharges: false, hasDiscounts: false, hasInstallments: false, hasSplit: false)
         
-        XCTAssertEqual(cardType, MLCardDrawerType.large)
+        XCTAssertEqual(cardType, MLCardDrawerTypeV3.large)
     }
     
     // Regular devices
     func testGetCardTypeForRegularWithEverything () {
         let cardType = PXCardSliderSizeManager.getCardTypeForContext(deviceSize: PXDeviceSize.Sizes.regular, hasCharges: true, hasDiscounts: true, hasInstallments: true, hasSplit: true)
         
-        XCTAssertEqual(cardType, MLCardDrawerType.small)
+        XCTAssertEqual(cardType, MLCardDrawerTypeV3.small)
     }
     
     func testGetCardTypeForRegularWithInstallmentsOnly () {
         let cardType = PXCardSliderSizeManager.getCardTypeForContext(deviceSize: PXDeviceSize.Sizes.regular, hasCharges: false, hasDiscounts: false, hasInstallments: true, hasSplit: false)
         
-        XCTAssertEqual(cardType, MLCardDrawerType.small)
+        XCTAssertEqual(cardType, MLCardDrawerTypeV3.large)
     }
     
     func testGetCardTypeForRegularWithChargesAndDiscountsOnly () {
         let cardType = PXCardSliderSizeManager.getCardTypeForContext(deviceSize: PXDeviceSize.Sizes.regular, hasCharges: true, hasDiscounts: true, hasInstallments: false, hasSplit: false)
         
-        XCTAssertEqual(cardType, MLCardDrawerType.large)
+        XCTAssertEqual(cardType, MLCardDrawerTypeV3.large)
     }
     
     func testGetCardTypeForRegularWithNothing () {
         let cardType = PXCardSliderSizeManager.getCardTypeForContext(deviceSize: PXDeviceSize.Sizes.regular, hasCharges: false, hasDiscounts: false, hasInstallments: false, hasSplit: false)
         
-        XCTAssertEqual(cardType, MLCardDrawerType.large)
+        XCTAssertEqual(cardType, MLCardDrawerTypeV3.large)
     }
     
     // Large devices
     func testGetCardTypeForLargeWithEverything () {
         let cardType = PXCardSliderSizeManager.getCardTypeForContext(deviceSize: PXDeviceSize.Sizes.large, hasCharges: true, hasDiscounts: true, hasInstallments: true, hasSplit: true)
         
-        XCTAssertEqual(cardType, MLCardDrawerType.large)
+        XCTAssertEqual(cardType, MLCardDrawerTypeV3.large)
     }
     
     func testGetCardTypeForLargeWithInstallmentsOnly () {
         let cardType = PXCardSliderSizeManager.getCardTypeForContext(deviceSize: PXDeviceSize.Sizes.large, hasCharges: false, hasDiscounts: false, hasInstallments: true, hasSplit: false)
         
-        XCTAssertEqual(cardType, MLCardDrawerType.large)
+        XCTAssertEqual(cardType, MLCardDrawerTypeV3.large)
     }
     
     func testGetCardTypeForLargeWithChargesAndDiscountsOnly () {
         let cardType = PXCardSliderSizeManager.getCardTypeForContext(deviceSize: PXDeviceSize.Sizes.large, hasCharges: true, hasDiscounts: true, hasInstallments: false, hasSplit: false)
         
-        XCTAssertEqual(cardType, MLCardDrawerType.large)
+        XCTAssertEqual(cardType, MLCardDrawerTypeV3.large)
     }
     
     func testGetCardTypeForLargeWithNothing () {
         let cardType = PXCardSliderSizeManager.getCardTypeForContext(deviceSize: PXDeviceSize.Sizes.large, hasCharges: false, hasDiscounts: false, hasInstallments: false, hasSplit: false)
         
-        XCTAssertEqual(cardType, MLCardDrawerType.large)
+        XCTAssertEqual(cardType, MLCardDrawerTypeV3.large)
     }
     
     // Extra large devices
     func testGetCardTypeForExtraLargeWithEverything () {
         let cardType = PXCardSliderSizeManager.getCardTypeForContext(deviceSize: PXDeviceSize.Sizes.extraLarge , hasCharges: true, hasDiscounts: true, hasInstallments: true, hasSplit: true)
         
-        XCTAssertEqual(cardType, MLCardDrawerType.large)
+        XCTAssertEqual(cardType, MLCardDrawerTypeV3.large)
     }
     
     func testGetCardTypeForExtraLargeWithInstallmentsOnly () {
         let cardType = PXCardSliderSizeManager.getCardTypeForContext(deviceSize: PXDeviceSize.Sizes.extraLarge, hasCharges: false, hasDiscounts: false, hasInstallments: true, hasSplit: false)
         
-        XCTAssertEqual(cardType, MLCardDrawerType.large)
+        XCTAssertEqual(cardType, MLCardDrawerTypeV3.large)
     }
     
     func testGetCardTypeForExtraLargeWithChargesAndDiscountsOnly () {
         let cardType = PXCardSliderSizeManager.getCardTypeForContext(deviceSize: PXDeviceSize.Sizes.extraLarge, hasCharges: true, hasDiscounts: true, hasInstallments: false, hasSplit: false)
         
-        XCTAssertEqual(cardType, MLCardDrawerType.large)
+        XCTAssertEqual(cardType, MLCardDrawerTypeV3.large)
     }
     
     func testGetCardTypeForExtraLargeWithNothing () {
         let cardType = PXCardSliderSizeManager.getCardTypeForContext(deviceSize: PXDeviceSize.Sizes.extraLarge, hasCharges: false, hasDiscounts: false, hasInstallments: false, hasSplit: false)
         
-        XCTAssertEqual(cardType, MLCardDrawerType.large)
+        XCTAssertEqual(cardType, MLCardDrawerTypeV3.large)
     }
 }
